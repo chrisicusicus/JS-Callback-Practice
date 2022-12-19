7d656c4e0cc413638344b5354e462113f5f4934f
chrisicusicus <115127962+chrisicusicus@users.noreply.github.com>
2022-12-19 17:54:02 -0500

    fish

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
-let direction = null;
-let x = 100;
-let y = 250;
-
-
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
@@ -18,6 +11,11 @@ move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
 
+
+const character = newImage('assets/green-character/static.gif')
+move(character).to(100, 250)
+let x = 100;
+let y = 250;
 
 function moveCharacter(){
 if(direction === 'west'){
@@ -36,50 +34,6 @@ character.style.left = x + 'px'
 character.style.bottom = y + 'px'
 }
 setInterval(moveCharacter, 1)
-
-
-document.addEventListener('keydown', function(e){
-    if(e.repeat) return;
-
-    if(e.key === 'ArrowLeft'){
-        direction = 'west'
-    }
-    if(e.key === 'ArrowUp'){
-        direction = 'north'
-    }
-    if(e.key === 'ArrowRight'){
-        direction = 'east'
-    }
-    if(e.key === 'ArrowDown'){
-        direction = 'south'
-    }
-})
-
-document.addEventListener('keyup', function(e){
-    direction = null
-})
-
-
-function handleDirectionChange(direction){
-    if(direction === null){
-        character.src = 'assets/green-character/static.gif'
-    }
-    if(direction === 'west'){
-        character.src = 'assets/green-character/west.gif'
-    }
-    if(direction === 'north'){
-        character.src = 'assets/green-character/north.gif'
-    }
-    if(direction === 'east'){
-        character.src = 'assets/green-character/east.gif'
-    }
-    if(direction === 'south'){
-        character.src = 'assets/green-character/south.gif'
-    }
-    
-}
-
-move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 function moveWithArrowKeys(left, bottom, callback){
     let direction = null;
@@ -104,8 +58,29 @@ document.addEventListener('keydown', function(e){
     callback(direction)
 })
 
+
 document.addEventListener('keyup', function(e){
     direction = null
     callback(direction)
 })
 
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+    
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
